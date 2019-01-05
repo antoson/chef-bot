7ed2ee596ae4bf3304743041848b2a0562d788f3
Ondřej Antoš <antos.ondrej@gmail.com>
2019-01-05 18:03:58 +0100

    recept api request

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client({disableEveryone: true});
 const prefix = "!";
 
+// https://api.edamam.com/search?app_id=453689ec&app_key=8aedd818b6528b232778a200cd2e6334&q=neco+necojinyho
+const recapi = "https://api.edamam.com/search?app_id=453689ec";
+const recapikey = "&app_key=8aedd818b6528b232778a200cd2e6334&to=1";
+
+//Request Api
+
+async function getRecipe(url) {
+    let edamanResponse = await fetch(url);
+    let response = await edamanResponse.json();
+
+    return res = response.hits[0].recipe.url;
+};
+
+//Client
 client.on("ready", async () => {
     console.log(`${client.user.username} is online!`);
 })
@@ -14,10 +28,17 @@ client.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+    let ingredients = args.join('+');
+    let url = recapi+recapikey+"&q="+ingredients;
+    let result = getRecipe(url);
 
     // !say xyz => xyz
     if(cmd === `${prefix}info`){
         return message.channel.send(`I'm running inside a Docker container!`);
+    }
+
+    if(cmd === `${prefix}recipe`){
+        return message.channel.send(`Your recipe: `);
     }
 })
 
